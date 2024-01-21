2780e02dba27fd16bb49029a5befbbda07920926
Shubham-Surywanshi <shubhamlatur2000@gmail.com>
2024-01-21 21:32:23 +0530

    removed comment from app.js

--- demorouting/src/App.js
+++ demorouting/src/App.js
@@ -1,23 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div >
-     <component1/>
+    <div>
+      <component1 />
     </div>
   );
 }
 
 export default App;
-     
 
-{/* <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/gallery" element={<Gallery />} />
-        </Routes>
-      </BrowserRouter> */}
+{
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/gallery" element={<Gallery />} />
+    </Routes>
+  </BrowserRouter>;
+}
